56f02310e38ca666ee24a0558c39badd0908b430
shivani <dhimanshivani67@gmail.com>
2018-06-12 17:29:44 +0530

    updated

--- database.php
+++ database.php
@@ -6,7 +6,7 @@
   $dbname= "crud";  //database name
 
 
-$conn = new mysqli($servername, $username, $password, $dbname);			// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);			// Create connection update
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);					// Check connection
